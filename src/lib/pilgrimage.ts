8d2a8a27f8d0b773ebed31bef86e2f8b709069a3
chxmq <yashsaraogi05@gmail.com>
2025-01-02 10:40:29 +0530

    Overwrite files with correct version

---
+++ src/lib/pilgrimage.ts
@@ -0,0 +1,50 @@
+export const achievements = [
+  {
+    src: "/cards/varanasi.jpg",
+    heading: "Varanasi",
+    description:
+      "Varanasi, the spiritual heart of India, lies on the Ganges, famed for its ghats, temples, and ancient heritage. A city of devotion and vibrant culture.",
+  },
+  {
+    src: "/cards/kedarnath.jpg",
+    heading: "Kedarnath",
+    description:
+      "Kedarnath, nestled in the Himalayas, is a revered Hindu pilgrimage site. Home to the sacred Kedarnath Temple, it offers stunning views, spiritual solace, and a rich cultural legacy.",
+  },
+  {
+    src: "/cards/madurai.jpg",
+    heading: "Madurai",
+    description:
+      "Madurai, known as the Temple City is famed for the iconic Meenakshi Amman Temple. A cultural hub in Tamil Nadu, it boasts ancient heritage, vibrant festivals, and rich South Indian traditions.",
+  },
+  {
+    src: "/cards/puri.jpg",
+    heading: "Puri",
+    description:
+      "Puri, a coastal city in Odisha, is renowned for the sacred Jagannath Temple and its annual Rath Yatra. Known as a Char Dham site, it also features golden beaches and vibrant spiritual traditions.",
+  },
+  {
+    src: "/cards/rameswaram.webp",
+    heading: "Rameswaram",
+    description:
+      "Rameswaram, located in Tamil Nadu, is a sacred pilgrimage site and part of the Char Dham. Known for the Ramanathaswamy Temple with its intricate corridors, it holds mythological significance and offers serene coastal beauty",
+  },
+  {
+    src: "/cards/g.webp",
+    heading: "The Golden Temple",
+    description:
+      "The Golden Temple, or Sri Harmandir Sahib, located in Amritsar, Punjab, is the holiest shrine of Sikhism. Renowned for its golden facade and serene Sarovar  it symbolizes peace, equality, and devotion, drawing millions of visitors worldwide.",
+  },
+  {
+    src: "/cards/A.jpg",
+    heading: "Ayodhya",
+    description:
+      "Ayodhya, in Uttar Pradesh, is a sacred city in Hinduism, believed to be the birthplace of Lord Rama. Rich in history and spirituality, it is home to the Ram Janmabhoomi Temple and ancient cultural heritage.",
+  },
+  {
+    src: "/cards/d.jpg",
+    heading: "Dwarka ",
+    description:
+      "Dwarka, in Gujarat, is one of Hinduism's Char Dham pilgrimage sites. Believed to be Lord Krishna's kingdom, it is famed for the Dwarkadhish Temple and its rich spiritual and historical significance.",
+  },
+];
